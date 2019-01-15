e9afee03b526fd475bd0a09db511654ad5dd62dc
jap86 <jasper.mollema@gmail.com>
2019-01-15 12:48:49 +0100

    Wat is je leeftijdprogramma

--- Dinsdag.java
+++ Dinsdag.java
@@ -1,10 +1,16 @@
-public class Dinsdag
-{
-	public static void main(String [] args)
-	{
-		System.out.println("Hello World");
-	
+import java.util.Scanner;
+public class Dinsdag{
+	public static void main(String [] args)	{
+		Scanner piet = new Scanner(System.in);
+		int leeftijd = piet.nextInt();
+		if(leeftijd < 16){
+			System.out.println("Je mag geen alchohol");
+		}
+		else if(leeftijd < 18){
+			System.out.println("Bier of wijn?");
+		}
+		else{
+			System.out.println("Bestel maar wat je wil!");
+		}
 	}
-
-
 }
